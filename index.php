e311567737938667fb2c5b756c309e6fac440755
hisyam44 <mohammadhisyam4@gmail.com>
2016-07-17 10:48:12 +0700

    add the fb api

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
-Hai 
+<html>
+       <head><title>HisyamApp</title></head>
+       <body>
+	<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '1751177035125579',
+      xfbml      : true,
+      version    : 'v2.7'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
+       </body>
+</html>
